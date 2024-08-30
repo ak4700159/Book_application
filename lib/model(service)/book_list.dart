81867eac185e2d4d5ea0dedc9227a056bfdf6199
ak47001 <ak47001@naver.com>
2024-08-30 16:03:24 +0900

    0.0.10 GetX start

---
+++ lib/model(service)/book_list.dart
@@ -0,0 +1,7 @@
+import 'package:book/model(service)/book.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+@riverpod
+class BookList extends _$BookList {}
+
+class _$BookList {}
